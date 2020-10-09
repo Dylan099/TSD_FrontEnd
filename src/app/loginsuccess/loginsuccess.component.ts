398cc473c3fe57c619a506a6dbc6516d79188313
Gadiel <gadi77altair.gabg@gmail.com>
2020-10-08 22:05:13 -0400

    creando validaciones

--- src/app/loginsuccess/loginsuccess.component.ts
+++ src/app/loginsuccess/loginsuccess.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-loginsuccess',
   templateUrl: './loginsuccess.component.html',
   styleUrls: ['./loginsuccess.component.css']
 })
-export class LoginsuccessComponent implements OnInit {
+export class FormComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    public fb: FormBuilder
+  ) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      company: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      url: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
-
+ ngOnInit() { }
+ saveData(){
+    console.log(this.myForm.value);
+  }
 }
