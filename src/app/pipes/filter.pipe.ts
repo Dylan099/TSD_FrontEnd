b1ed199cd57c4483a786118b97d04e77699764bd
FerchitaSM <45465803+FerchitaSM@users.noreply.github.com>
2020-12-03 22:59:19 -0400

    Moviendo el mapa

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
+  
   transform(value: any, arg: any): any {
     const resultPost = [];
     for(const post of  value){
